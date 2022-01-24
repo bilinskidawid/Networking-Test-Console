2a9c02fd567c678bc2709562a6d9e6dcf82a2e16
Dawid <bilinskidawid5@gmail.com>
2022-01-24 21:32:43 +0000

    made it so that u put in two integers and they get put into one "message" that is then sent.
    issue with instances inside loops not accesssible outside

--- Program.cs
+++ Program.cs
@@ -10,41 +10,62 @@ namespace Networking_Test_Console
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-        connection:
-            try
-            {
-                TcpClient client = new TcpClient("127.0.0.1", 1333);
-            senddata:
-                string messageToSend = Console.ReadLine();
-                int byteCount = Encoding.ASCII.GetByteCount(messageToSend + 1);
-                byte[] sendData = Encoding.ASCII.GetBytes(messageToSend);
 
-                NetworkStream stream = client.GetStream(); //opens memory slot for the stream data
-                stream.Write(sendData, 0, sendData.Length); //writes the bytes from 0 to end
+
+            bool notconnected = true;
+            //while (notconnected)
+            //{
+               // try
+                //{
+                    TcpClient client = new TcpClient("127.0.0.1", 1333);
+                    NetworkStream stream = client.GetStream();
+                    notconnected = false;
+                //}
+               // catch (Exception e)
+               // {
+               ///     Console.WriteLine("Couldn't connect");
+               // }
+           // }
+
+            send(50, 100, client, stream);
+            close(client, stream);
+
+            
+            
+            
+            
+            
+            
+            static void send(int a, int b, TcpClient client, NetworkStream stream)
+            {
+                byte[] x = new byte[4];
+                x = BitConverter.GetBytes(50);
+                byte[] y = new byte[4];
+                y = BitConverter.GetBytes(100);
+
+                byte[] msg = new byte[8];
+                Buffer.BlockCopy(x, 0, msg, 0, 4);
+                Buffer.BlockCopy(y, 0, msg, 4, 4);
+
+
+
+                //opens memory slot for the stream data
+                stream.Write(msg, 0, msg.Length); //writes the bytes from 0 to end
                 Console.WriteLine("Pass: Sent");
 
-                StreamReader sr = new StreamReader(stream);
-                string response = sr.ReadLine();
-                Console.WriteLine(response);
-                string msg = Console.ReadLine();
-                if (msg == "")
-                {
-
-                    goto close;
-                }
-                goto senddata;
-
-            close:
+            }
+               
+            
+            static void close(TcpClient client, NetworkStream stream)
+            {
                 stream.Close();
                 client.Close();
-                Console.ReadKey();
+            }
 
+                }
+                
             }
-            catch (Exception e)
-            {
-                Console.WriteLine("failed to connect...");
-                goto connection; //retries connection if failed
             }
-        }
-    }
-}
+       // }
+   // }
+
