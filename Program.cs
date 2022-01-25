a4a7f646fe440575518d1cb12cec16670f273a4e
Dawid <bilinskidawid5@gmail.com>
2022-01-25 21:03:12 +0000

    Appears to be working, need to implement this code into the xamarin code

--- Program.cs
+++ Program.cs
@@ -11,23 +11,30 @@ namespace Networking_Test_Console
         {
             Console.WriteLine("Hello World!");
 
-
+            TcpClient client = null;
+            NetworkStream stream = null;
+            StreamReader sr = null;
             bool notconnected = true;
-            //while (notconnected)
-            //{
-               // try
-                //{
-                    TcpClient client = new TcpClient("127.0.0.1", 1333);
-                    NetworkStream stream = client.GetStream();
-                    notconnected = false;
-                //}
-               // catch (Exception e)
-               // {
-               ///     Console.WriteLine("Couldn't connect");
-               // }
-           // }
+            while (notconnected)
+            {
+                System.Threading.Thread.Sleep(4000); //retries every 4 seconds
+                try
+                {
+                    client = new TcpClient("127.0.0.1", 1333);
+                    stream = client.GetStream();
+                    sr = new StreamReader(stream);
+                    notconnected = false; //if it does connect without throwing errors, it can exit the while loop and carry on with the program.
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Couldn't connect"); //will loop until connects
+                }
+            }
+
 
             send(50, 100, client, stream);
+            send(51, 130, client, stream);
+            send(20, 150, client, stream);
             close(client, stream);
 
             
@@ -39,9 +46,9 @@ namespace Networking_Test_Console
             static void send(int a, int b, TcpClient client, NetworkStream stream)
             {
                 byte[] x = new byte[4];
-                x = BitConverter.GetBytes(50);
+                x = BitConverter.GetBytes(a);
                 byte[] y = new byte[4];
-                y = BitConverter.GetBytes(100);
+                y = BitConverter.GetBytes(b);
 
                 byte[] msg = new byte[8];
                 Buffer.BlockCopy(x, 0, msg, 0, 4);
@@ -52,7 +59,8 @@ namespace Networking_Test_Console
                 //opens memory slot for the stream data
                 stream.Write(msg, 0, msg.Length); //writes the bytes from 0 to end
                 Console.WriteLine("Pass: Sent");
-
+                stream.Flush();
+                
             }
                
             
@@ -66,6 +74,4 @@ namespace Networking_Test_Console
                 
             }
             }
-       // }
-   // }
-
+      
